5a1df83fbea3957b1198e1695f215d092106dea7
DATUK SI PENAKLUK API <150427432+DatukDev@users.noreply.github.com>
2024-12-04 21:28:59 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    email: 'noval@fun88.com',
+    password: 'asd123',
+    telegramToken: '6909631780:AAEPEZFP_S0XiSDnPVX1C8NTiW_mgVpmdMQ',
+    database: {
+        host: '193.203.166.203',
+        user: 'u219913611_root',
+        password: 'Ilau6t8&q8j/',
+        database: 'u219913611_blaster'
+    }
+};
